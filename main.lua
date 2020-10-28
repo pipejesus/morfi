d1098b443513b3ae33bdc62cba51a4913c225835
Grzegorz Jasiński <grzegorz.jasinski@polcode.net>
2020-10-28 21:55:39 +0100

    make thiz work

--- main.lua
+++ main.lua
@@ -1,46 +1,58 @@
 local tween = require('tween')
-Rows = 5
+local Playaz = require('playaz')
+local Frames = require('frames')
+local FramesInterval = 1                 -- 2 seconds
+local PlayazEasing = tween.easing.inCirc
+local PlayazTweenDuration = 0.04
+
+Rows = 7
 Cols = 5
-DrawStartX, DrawStartY = 100, 100
+DrawStartX, DrawStartY = 200, 200
 SquareWidth, SquareHeight = 50, 50
 CurrentFrame = 1
 TimePassed = 0
 
-OldPlayaX = 0
-OldPlayaY = 0
-
-Playaz = { 'a', 'b', 'c', 'd', 'e', 'f'}
-Frames = {
-  {
-    ' ', ' ', 'f', ' ', ' ', 
-    ' ', 'a', 'e', ' ', ' ', 
-    ' ', 'b', ' ', ' ', ' ', 
-    ' ', 'c', ' ', ' ', ' ', 
-    ' ', 'd', ' ', ' ', ' ',   
-  },
-  {
-    ' ', ' ', ' ', ' ', ' ', 
-    'f', 'e', 'e', ' ', ' ', 
-    ' ', ' ', 'a', 'b', ' ', 
-    ' ', ' ', ' ', 'c', ' ', 
-    ' ', ' ', ' ', 'd', ' ',   
-  },
-}
 
 function love.load()
-  OldPlayaX, OldPlayaY = GetPlayaPositionCalculated('a', Frames[1])  
+  for playaName, playaDef in pairs(Playaz) do        
+    Playaz[playaName].current.x, Playaz[playaName].current.y = GetPlayaPositionCalculated(playaName, Frames[CurrentFrame])
+    Playaz[playaName].nextTarget.x, Playaz[playaName].nextTarget.y = Playaz[playaName].current.x, Playaz[playaName].current.y
+    Playaz[playaName].t = tween.new(PlayazTweenDuration, Playaz[playaName].current, Playaz[playaName].nextTarget, PlayazEasing)
+  end
 end
 
 function love.update(dt) 
-  HandleTimePassed(dt)
+  HandleTimePassedForFrames(dt)
+  UpdatePlayazTweensTargets(dt)
+  ProceedTweensInTime(dt)
 end
 
-function HandleTimePassed(dt)
+function ProceedTweensInTime(dt)
+  for playaName, playaDef in pairs(Playaz) do  
+    Playaz[playaName].t:update(dt)
+  end
+end
+
+function UpdatePlayazTweensTargets(dt)
+  for playaName, playaDef in pairs(Playaz) do  
+    Playaz[playaName].t = tween.new(PlayazTweenDuration, Playaz[playaName].current, Playaz[playaName].nextTarget, PlayazEasing)
+  end
+end
+
+function HandleTimePassedForFrames(dt)
   TimePassed = TimePassed + dt    
-  if TimePassed >= 2 then
-    TimePassed = TimePassed - 2
+  if TimePassed >= FramesInterval then
+    TimePassed = TimePassed - FramesInterval
     SwitchToNextFrame()
-  end  
+    SwitchTweensToNextFrame()
+  end
+end
+
+function SwitchTweensToNextFrame()
+  for playaName, playaDef in pairs(Playaz) do  
+    -- Playaz[playaName].current.x, Playaz[playaName].current.y = Playaz[playaName].nextTarget.x, Playaz[playaName].nextTarget.y
+    Playaz[playaName].nextTarget.x, Playaz[playaName].nextTarget.y = GetPlayaPositionCalculated(playaName, Frames[CurrentFrame])
+  end
 end
 
 function SwitchToNextFrame()
@@ -55,28 +67,40 @@ end
 
 function GetPlayaPosition(playaName, frame)
   local x, y, i = nil, nil, nil
-  for idx, val in ipairs(frame) do
-    if val == playaName then
-      i = idx
+  for yy, valY in ipairs(frame) do
+    for xx, valX in ipairs(valY) do
+      if playaName == frame[yy][xx] then
+        x = xx
+        y = yy
+      end
     end
-  end  
-  x = i % Cols
-  y = math.floor(i / Rows)
-  return x,y
+  end
+  return x, y
 end
 
 function GetPlayaPositionCalculated(playa, frame)
   local x,y = GetPlayaPosition(playa, frame)
-  local calcX = (x - 1 ) * SquareWidth  + DrawStartX
-  local calcY = (y - 1 ) * SquareHeight + DrawStartY  
+  local calcX = (x - 1) * SquareWidth  + DrawStartX
+  local calcY = (y - 1) * SquareHeight + DrawStartY  
   return calcX, calcY
 end
 
-function DrawSquare(x, y)
+function DrawSquare(playaName, colors)  
+  -- local x,y = GetPlayaNamePositionCalculated(playa, Frames[CurrentFrame])
+  -- Playaz[playa].x,  Playaz[playa].y = y  
+  local x, y = Playaz[playaName].current.x, Playaz[playaName].current.y
+  love.graphics.setColor(colors)
+  love.graphics.rectangle('fill', x, y, SquareWidth, SquareHeight)
 end
 
-function DrawAllSquares()
-  local x,y = GetPlayaPositionCalculated('a', Frames[CurrentFrame])  
-  love.graphics.setColor(0.4, 0.1, 0.23, 0.9)
-  love.graphics.rectangle('fill', x, y, 50, 50)
+function DrawAllSquares()  
+    DrawSquare('a', {0.4, 0.1, 0.23, 0.9})
+    DrawSquare('b', {0.1, 0.3, 0.33, 0.9})  
+    DrawSquare('c', {0.4, 0.0, 0.33, 0.9})  
+    DrawSquare('d', {0.7, 0.5, 0.33, 0.9})  
+    DrawSquare('e', {0.3, 0.7, 0.0, 0.9})  
+    DrawSquare('f', {0.17, 0.7, 0.33, 0.9})
+    DrawSquare('g', {0.93, 0.1, 0.33, 0.9})
+    DrawSquare('h', {0.07, 0.57, 0.16, 0.9})
+    DrawSquare('i', {0.43, 0.27, 0.46, 0.9})
 end
---
+++ playaz.lua
@@ -0,0 +1,101 @@
+return { 
+  a = {    
+    t = nil,
+    current = {
+      x = nil,
+      y = nil,
+    },
+    nextTarget = {
+      x = nil,
+      y = nil
+    }
+  },
+  b = {
+    t = nil,
+    current = {
+      x = nil,
+      y = nil,
+    },
+    nextTarget = {
+      x = nil,
+      y = nil
+    }
+  },
+  c = {    
+    t = nil,
+    current = {
+      x = nil,
+      y = nil,
+    },
+    nextTarget = {
+      x = nil,
+      y = nil
+    }
+  },
+  d = {
+    t = nil,
+    current = {
+      x = nil,
+      y = nil,
+    },
+    nextTarget = {
+      x = nil,
+      y = nil
+    }
+  },
+  e = {    
+    t = nil,
+    current = {
+      x = nil,
+      y = nil,
+    },
+    nextTarget = {
+      x = nil,
+      y = nil
+    }
+  },
+  f = {    
+    t = nil,
+    current = {
+      x = nil,
+      y = nil,
+    },
+    nextTarget = {
+      x = nil,
+      y = nil
+    }
+  },
+  g = {    
+    t = nil,
+    current = {
+      x = nil,
+      y = nil,
+    },
+    nextTarget = {
+      x = nil,
+      y = nil
+    }
+  },
+  h = {    
+    t = nil,
+    current = {
+      x = nil,
+      y = nil,
+    },
+    nextTarget = {
+      x = nil,
+      y = nil
+    }
+  },
+  i = {    
+    t = nil,
+    current = {
+      x = nil,
+      y = nil,
+    },
+    nextTarget = {
+      x = nil,
+      y = nil
+    }
+  },     
+}
